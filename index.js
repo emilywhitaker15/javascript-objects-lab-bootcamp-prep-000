01fa4f4a9b254cd6307555fa842fffe9a3343170
emilywhitaker15@gmail.com <emilywhitaker15@gmail.com>
2018-05-21 14:32:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function Objects() {
+
   var recipes = {
     flour: "1 cup",
     sugar: "1 cup"
   }
-}
+
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
